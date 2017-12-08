f1f8ec3c95ed66a362316235847a0c5bf280b25a
jdosk <jdosk314@mail.endicott.edu>
2017-12-08 16:23:25 -0500

    Updated sport model

--- src/com/endicott/edu/models/SportModel.java
+++ src/com/endicott/edu/models/SportModel.java
@@ -12,7 +12,8 @@ public class SportModel implements Serializable {
     private int maxPlayers = 0;
     private int costPerDay = 0;
     private int hourLastUpdated = 0;
-    private int reputation = 100;
+    private int reputation = 50;
+    private int overallRep = 0;
     private int gamesWon = 0;
     private int gamesLost = 0;
     private int gamesTied = 0;
@@ -198,5 +199,8 @@ public class SportModel implements Serializable {
 
     public void setGender(String gender) { this.gender = gender; }
 
-}
+    public int getOverallRep() { return overallRep; }
 
+    public void setOverallRep(int overallRep) { this.overallRep = overallRep; }
+
+}
