2433934c7bc1d5262c7eeaa2d16ab91dcb5d53d9
Jomuss <36137125+Jomuss@users.noreply.github.com>
2018-10-01 14:46:19 -0400

    Added name to about.jsp

---
+++ src/com/endicott/edu/models/PopupEventModel.java
@@ -0,0 +1,60 @@
+package com.endicott.edu.models;
+
+import java.io.Serializable;
+
+/**
+ * Created by CJ Mustone and Joseph Moss 10/01/18
+ */
+public class PopupEventModel implements Serializable {
+    private String type;
+    private boolean response;
+    private String title;
+    private String description;
+    private String goodButtonText;
+    private String badButtonText;
+
+    /**
+     *
+     * @param type
+     * @param title
+     * @param description
+     * @param goodButtonText
+     * @param badButtonText
+     */
+    PopupEventModel(String type, String title, String description, String goodButtonText, String badButtonText){
+        this.type = type;
+        this.title = title;
+        this.description = description;
+        this.goodButtonText = goodButtonText;
+        this.badButtonText = badButtonText;
+        this.response = false;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isResponse() {
+        return response;
+    }
+    public boolean getResponse() {
+        return response;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGoodButtonText() {
+        return goodButtonText;
+    }
+
+    public String getBadButtonText() {
+        return badButtonText;
+    }
+}
