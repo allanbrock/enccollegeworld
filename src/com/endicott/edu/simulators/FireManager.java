fdd10434036352c9b584cfaa831e5b773dbb32a4
cjmustone <cmust116@mail.endicott.edu>
2018-10-22 23:15:26 -0400

    removing students who die in fires and trying to get the list of students removed to display.

--- src/com/endicott/edu/simulators/FireManager.java
+++ src/com/endicott/edu/simulators/FireManager.java
@@ -3,6 +3,7 @@ package com.endicott.edu.simulators;
 import com.endicott.edu.datalayer.BuildingDao;
 import com.endicott.edu.datalayer.DormitoryDao;
 import com.endicott.edu.datalayer.FireDAO;
+import com.endicott.edu.datalayer.StudentDao;
 import com.endicott.edu.models.*;
 
 import java.util.ArrayList;
@@ -12,7 +13,6 @@ import java.util.Random;
 public class FireManager {
     FireDAO fireDao = new FireDAO();
     BuildingDao buildingDao = new BuildingDao();
-    //DormitoryDao dormDao = new DormitoryDao();
 
 
 
@@ -22,8 +22,8 @@ public class FireManager {
         possiblyCreateFire(runId,hoursAlive);
         List<FireModel> fires = FireDAO.getFires(runId);
 
+        // alert popupManager about each fire, currently only one exists fire at a time
         if (fires.size() != 0) {
-            // alert popupManager about each fire
             for (FireModel fire : fires) {
                 popupManager.newPopupEvent("Fire in " + fire.getBuildingBurned().getName(), fire.getDescription(), "ok", "done", "resources/images/fire.png", "Plague Doctor");
             }
@@ -45,15 +45,37 @@ public class FireManager {
      */
     public void startFireRandomly(String runId,int hoursAlive){
         ArrayList<BuildingModel> buildings = (ArrayList<BuildingModel>) buildingDao.getBuildings(runId);
+        ArrayList<StudentModel> students = (ArrayList<StudentModel>) StudentDao.getStudents(runId);
+        ArrayList<StudentModel> fireVictims = new ArrayList<>();
+
         Random rand = new Random();
         int randomIndex = rand.nextInt(buildings.size());
         BuildingModel buildingToBurn = buildings.get(randomIndex);
 
         FireModel fire = new FireModel(getFireCost(), getNumFatalities(), runId, buildingToBurn);
+
+        if (buildingToBurn.getNumStudents() > 0 && getNumFatalities() > 0){
+            for (int i =0; i == getNumFatalities(); i++){
+                int studentIndex = rand.nextInt(students.size());
+                StudentModel victim = students.get(studentIndex);
+                fireVictims.add(victim);
+                students.remove(studentIndex);
+
+            }
+            fire.setDescription(fireVictims);
+        } else{
+            fire.setNumOfFatalities(0);
+        }
+
         FireDAO dao = new FireDAO();
+        StudentDao studentDao = new StudentDao();
         List<FireModel> fires = FireDAO.getFires(runId);
         fires.add(fire);
         dao.saveNewFire(runId,fire);
+        studentDao.saveAllStudents(runId,students);
+        fireVictims.clear();
+
+
 
         NewsManager.createNews(runId, hoursAlive, "Fire detected at " + fire.getBuildingBurned().getName(), NewsType.COLLEGE_NEWS, NewsLevel.BAD_NEWS);
     }
