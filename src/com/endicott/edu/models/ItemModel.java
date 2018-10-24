c8ba0b61aab59f30d017e0f8f0349c09bdc65d6f
stevesuchcicki <ssuch855@mail.endicott.edu>
2018-10-24 12:26:53 -0400

    Sprint 4: Added more to the viewstore.jsp and added some logic to the Inventory Manager

--- src/com/endicott/edu/models/ItemModel.java
+++ src/com/endicott/edu/models/ItemModel.java
@@ -8,13 +8,17 @@ import java.io.Serializable;
 public class ItemModel implements Serializable {
     private String name;
     private Boolean isPurchased;
+    private String imageName;
+    private int cost;
 
     public ItemModel() {
     }
 
-    public ItemModel(String name, Boolean isPurchased) {
+    public ItemModel(String name, Boolean isPurchased, String imageName, int cost) {
         this.name = name;
         this.isPurchased = isPurchased;
+        this.imageName = imageName;
+        this.cost = cost;
     }
 
     public String getName() {
@@ -31,6 +35,22 @@ public class ItemModel implements Serializable {
 
     public void setPurchased(Boolean purchased) {
         isPurchased = purchased;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
     }
 }
 
--- src/com/endicott/edu/simulators/CollegeManager.java
+++ src/com/endicott/edu/simulators/CollegeManager.java
@@ -67,6 +67,9 @@ public class CollegeManager {
 
         GateManager.establishCollege(collegeId);
 
+        InventoryManager inventoryManager = new InventoryManager();
+        inventoryManager.establishCollege(collegeId);
+
         return college;
     }
 
