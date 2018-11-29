1a6dc07e1793c594e401face3a8675956fbfe46e
Jeremy Barr <jbarr811@mail.endicott.edu>
2018-11-29 15:17:39 -0500

    updated gates WIP

--- src/com/endicott/edu/models/GateModel.java
+++ src/com/endicott/edu/models/GateModel.java
@@ -6,14 +6,14 @@ public class GateModel implements Serializable {
     private String  key;
     private String  description;
     private String  iconPath;
-    private int     goal;
+    private int     level;
     private String  runId = "unknown";
 
-    public GateModel(String _key, String _description, String _iconPath, int _gate){
+    public GateModel(String _key, String _description, String _iconPath, int _level){
             this.key            = _key;
             this.description    = _description;
             this.iconPath       = _iconPath;
-            this.goal           = _gate;
+            this.level          = _level;
     };
 
     public String getKey() { return this.key; }
@@ -22,7 +22,7 @@ public class GateModel implements Serializable {
 
     public String getIconPath() { return this.iconPath; }
 
-    public int getGoal() { return this.goal; }
+    public int getLevel() { return this.level; }
 
     public void setRunId(String _runId) { this.runId = _runId; }
 }
