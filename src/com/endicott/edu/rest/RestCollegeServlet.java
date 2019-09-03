20d8057b52bb66b815df06b7d31833e2c6a86fef
allanbrock <abrocken@endicott.edu>
2019-09-03 19:58:09 -0400

    Added restful interface for college.

---
+++ src/com/endicott/edu/rest/RestCollegeServlet.java
@@ -0,0 +1,50 @@
+package com.endicott.edu.rest;
+
+import com.endicott.edu.datalayer.CollegeDao;
+import com.endicott.edu.models.CollegeModel;
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+public class RestCollegeServlet extends javax.servlet.http.HttpServlet {
+    private static Logger logger = Logger.getLogger("ViewAboutServlet");
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String[] splits = pathInfo.split("/");
+        if(splits.length < 2) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String collegeId = splits[1];
+        CollegeModel college;
+        college = CollegeDao.getCollege(collegeId);
+
+        sendAsJson(response, college);
+    }
+
+    //a utility method to send object
+    //as JSON response
+    private void sendAsJson(
+            HttpServletResponse response,
+            Object obj) throws IOException {
+
+        response.setContentType("application/json");
+        Gson gson = new Gson();
+        String res = gson.toJson(obj);
+
+        PrintWriter out = response.getWriter();
+
+        out.print(res);
+        out.flush();
+    }
+}
