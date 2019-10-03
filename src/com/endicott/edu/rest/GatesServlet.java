d558af326e3ab2281322ca807be30382f6e98a6a
ebawmann <ebawm372@mail.endicott.edu>
2019-10-03 14:03:05 -0400

    Added Gates (Objectives) Servlet

---
+++ src/com/endicott/edu/rest/GatesServlet.java
@@ -0,0 +1,57 @@
+package com.endicott.edu.rest;
+
+import com.endicott.edu.datalayer.GateDao;
+import com.endicott.edu.models.GateModel;
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+/**
+ * Created by Elijah Bawmann on 10/03/2019.
+ */
+
+public class GatesServlet extends javax.servlet.http.HttpServlet {
+    private static Logger logger = Logger.getLogger("ViewAboutServlet");
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String[] splits = pathInfo.split("/");
+        if(splits.length < 2) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String collegeId = splits[1];
+        GateModel[] gates;
+        gates = GateDao.getGatesArray(collegeId);
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+
+        sendAsJson(response, gates);
+    }
+
+
+    private void sendAsJson(
+            HttpServletResponse response,
+            Object obj) throws IOException {
+
+        response.setContentType("application/json");
+        Gson gson = new Gson();
+        String res = gson.toJson(obj);
+
+        PrintWriter out = response.getWriter();
+
+        out.print(res);
+        out.flush();
+    }
+}
