f16b571ad46c7dc428e521d5a69e416c26a6cd38
allanbrock <abrocken@endicott.edu>
2019-10-13 16:54:17 -0400

    Adds doPost method to college rest interface.

--- src/com/endicott/edu/rest/RestCollegeServlet.java
+++ src/com/endicott/edu/rest/RestCollegeServlet.java
@@ -8,7 +8,10 @@ import com.endicott.edu.simulators.CollegeManager;
 import com.endicott.edu.simulators.PopupEventManager;
 import com.google.gson.Gson;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Logger;
@@ -16,7 +19,60 @@ import java.util.logging.Logger;
 public class RestCollegeServlet extends javax.servlet.http.HttpServlet {
     private static Logger logger = Logger.getLogger("RestCollegeServlet");
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("In RestCollegeServlet doPut");
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String[] splits = pathInfo.split("/");
+        if(splits.length < 2) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String collegeId = splits[1];
+        CollegeModel college;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+
+        // There may be a command after the college name.
+        if (splits.length >= 3) {
+            String command = splits[2];  // only command is next day at the moment.
+            logger.info("Servlet command: " + command);
+            PopupEventManager popupMgr = new PopupEventManager();  // This is a stub.  Must be changed.
+            CollegeManager.advanceTimeByOneDay(collegeId, popupMgr);
+
+            EverythingModel everything = EverythingDao.getEverything(collegeId);
+            sendAsJson(response, everything);
+            return;
+        }
+
+        logger.info("In RestCollegeServlet doPut " + collegeId);
+
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+
+        String payload = buffer.toString();
+        Gson gson = new Gson();
+        CollegeModel collegeModel = gson.fromJson(payload, CollegeModel.class);
+        logger.info("Attempting to save tuition. " + collegeId);
+        CollegeDao.saveCollege(collegeModel);
+
+        sendAsJson(response, collegeModel);
+    }
+
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        logger.info("In RestCollegeServlet doGet");
         String pathInfo = request.getPathInfo();
         if (pathInfo == null) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST);
