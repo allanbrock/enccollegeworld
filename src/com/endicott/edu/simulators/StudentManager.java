3992874de81993432b8ed974714d50ac1188cf4a
timajmello <timjamello@icloud.com>
2019-10-15 16:30:31 -0400

    Student Feedback now updates more than just once.

--- src/com/endicott/edu/simulators/StudentManager.java
+++ src/com/endicott/edu/simulators/StudentManager.java
@@ -359,6 +359,7 @@ public class StudentManager {
         int diningHallHappinessLevel = SimulatorUtilities.getRandomNumberWithNormalDistribution(diningHallQuality, 15, 0, 100);
 
         student.setDiningHallHappinessRating(diningHallHappinessLevel);
+        getStudentFeedback(student, college.getRunId());
     }
 
     private void setAcademicCenterHappinessRating(StudentModel student, CollegeModel college) {
@@ -367,6 +368,7 @@ public class StudentManager {
         int acadmicBuildingHappinessLevel = SimulatorUtilities.getRandomNumberWithNormalDistribution(academicBuildingQuality, 15, 0, 100);
 
         student.setAcademicCenterHappinessRating(acadmicBuildingHappinessLevel);
+        getStudentFeedback(student, college.getRunId());
     }
 
     private void setDormHappinessRating(StudentModel student,CollegeModel college){
@@ -375,6 +377,7 @@ public class StudentManager {
         int dormHappinessLevel = SimulatorUtilities.getRandomNumberWithNormalDistribution(dormQuality, 15, 0, 100);
 
         student.setDormHappinessRating(dormHappinessLevel);
+        getStudentFeedback(student, college.getRunId());
     }
 
     private void setStudentFunHappiness(StudentModel s, CollegeModel college) {
@@ -421,6 +424,7 @@ public class StudentManager {
         happinessRating = Math.max(happinessRating, 0);
         happinessRating = Math.min(happinessRating, 100);
         s.setAdvisorHappinessHappinessRating(happinessRating);
+        getStudentFeedback(s, college.getRunId());
     }
 
     private void setStudentAcademicHappiness(StudentModel s, CollegeModel college) {
@@ -434,12 +438,14 @@ public class StudentManager {
         happinessRating = Math.min(happinessRating, 100);
 
         s.setAcademicHappinessRating(happinessRating);
+        getStudentFeedback(s, college.getRunId());
     }
 
     private void setStudentProfessorHappiness(String collegeId, StudentModel s){
         int rating = FacultyManager.getAverageFacultyPerformance(collegeId);
         int happinessRating = SimulatorUtilities.getRandomNumberWithNormalDistribution(rating, 10, 0, 100);
         s.setProfessorHappinessRating(happinessRating);
+        getStudentFeedback(s, college.getRunId());
     }
 
     private void setStudentHealthHappiness(StudentModel s) {
@@ -514,6 +520,7 @@ public class StudentManager {
         buildingHappinessLevel = SimulatorUtilities.getRandomNumberWithNormalDistribution(avgBuildingQuality, 15, 0, 100) + entertainmentHappiness;
         buildingHappinessLevel = Math.min(buildingHappinessLevel, 100);
         s.setOverallBuildingHappinessRating(buildingHappinessLevel);
+        getStudentFeedback(s, college.getRunId());
     }
 
     private void calculateStudentFacultyRatio(String collegeId) {
