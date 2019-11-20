469dca5ef63ae2e8c038b5afc5d680f5386e4945
jaredserinoendicott <47423407+jaredserinoendicott@users.noreply.github.com>
2019-11-20 12:29:22 -0500

    Testing for Allen

--- src/com/endicott/edu/rest/BuildingServlet.java
+++ src/com/endicott/edu/rest/BuildingServlet.java
@@ -4,6 +4,7 @@ import com.endicott.edu.datalayer.BuildingDao;
 import com.endicott.edu.datalayer.CollegeDao;
 import com.endicott.edu.models.BuildingModel;
 import com.endicott.edu.models.CollegeModel;
+import com.endicott.edu.simulators.BuildingManager;
 import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ public class BuildingServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         String pathInfo = request.getPathInfo();
+        logger.info("TEST" + pathInfo);
         if (pathInfo == null) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
@@ -34,6 +36,18 @@ public class BuildingServlet extends javax.servlet.http.HttpServlet {
         String collegeId = splits[1];
         BuildingModel[] buildings;
         buildings = BuildingDao.getBuildingsArray(collegeId);
+
+        for(int i = 0; i < splits.length; i++) {
+            logger.info(i + " " + splits[i]);
+        }
+
+        if(splits[2] == "repair") {
+            BuildingManager.repairBuilding(collegeId, buildings[0]);
+        } else if (splits[2] == "upgrade") {
+
+        } else if (splits[2] == "purchase") {
+
+        }
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
