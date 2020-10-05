fb53e3d98e0a328b7ea10b61c875ef2de2352a16
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-05 23:35:32 +0530

    Changed the upgrades so they work for the correct administrative building model

--- src/com/endicott/edu/models/AdminModel.java
+++ src/com/endicott/edu/models/AdminModel.java
@@ -4,31 +4,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class AdminModel implements Serializable{
-    private ArrayList<Upgrades> upgrades;
-    public static final int maxLevel = 3;
-    private Upgrades tables = new Upgrades("Tables",0, 0);
-    private Upgrades bursars = new Upgrades("Bursars", 0, 0);
-    private Upgrades cubicles = new Upgrades("cubicles", 0, 0);
-    private Upgrades meetingHalls = new Upgrades("meetingHalls", 0, 0);
 
     public AdminModel() {
-        this.upgrades.add(tables);
-        this.upgrades.add(bursars);
-        this.upgrades.add(cubicles);
-        this.upgrades.add(meetingHalls);
+
     }
 
-    public void upgradeAdminModel(Upgrades upgrade) {
-        for(int i = 0; i < maxLevel; i++) {
-            if(upgrade.getName()==upgrades.get(i).getName())
-                upgrades.get(i).increaseLevel();
-        }
-    }
-
-    public void downgradeAdminModel(Upgrades upgrade) {
-        for(int i = 0; i < maxLevel; i++) {
-            if(upgrade.getName()==upgrades.get(i).getName())
-                upgrades.get(i).decreaseLevel();
-        }
-    }
 }
--- src/com/endicott/edu/models/AdministrativeBldgModel.java
+++ src/com/endicott/edu/models/AdministrativeBldgModel.java
@@ -1,8 +1,36 @@
 package com.endicott.edu.models;
 
+import java.util.ArrayList;
+
 public class AdministrativeBldgModel extends BuildingModel{
     //inherits from BuildingModel
+
+    private ArrayList<Upgrades> upgrades;
+    public static final int maxLevel = 3;
+    private Upgrades tables = new Upgrades("Tables",0, 0);
+    private Upgrades bursars = new Upgrades("Bursars", 0, 0);
+    private Upgrades cubicles = new Upgrades("cubicles", 0, 0);
+    private Upgrades meetingHalls = new Upgrades("meetingHalls", 0, 0);
+
     public AdministrativeBldgModel(String name){
         super(name, BuildingType.admin().getType());
+        this.upgrades.add(tables);
+        this.upgrades.add(bursars);
+        this.upgrades.add(cubicles);
+        this.upgrades.add(meetingHalls);
+    }
+
+    public void upgradeAdminModel(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).increaseLevel();
+        }
+    }
+
+    public void downgradeAdminModel(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).decreaseLevel();
+        }
     }
 }
