b6847d1c4d961f6abb1fd7b346079ac9cf7feaf4
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-05 14:16:01 +0530

    Added upgrades to AcademicCenter, Admin and Entertainment. Added upgrade and downgrade methods to Dining and Dorm building types.

--- src/com/endicott/edu/models/AcademicCenterModel.java
+++ src/com/endicott/edu/models/AcademicCenterModel.java
@@ -1,8 +1,36 @@
 package com.endicott.edu.models;
 
+import java.util.ArrayList;
+
 public class AcademicCenterModel extends BuildingModel {
+    private ArrayList<Upgrades> upgrades;
+    public static final int maxLevel = 10;
+    private Upgrades tables = new Upgrades("Tables",0, 0);
+    private Upgrades airconditioning = new Upgrades("Air Conditioning", 0, 0);
+    private Upgrades vendingMachines = new Upgrades("Vending Machines", 0, 0);
+    private Upgrades projectors = new Upgrades("Projectors", 0, 0);
+    private Upgrades seats = new Upgrades("Seats", 0, 0);
     //inherits from BuildingModel
     public AcademicCenterModel(String name, int numStudents, String size){
         super(name, numStudents, BuildingType.academic().getType(), size);
+        this.upgrades.add(tables);
+        this.upgrades.add(airconditioning);
+        this.upgrades.add(vendingMachines);
+        this.upgrades.add(projectors);
+        this.upgrades.add(seats);
+    }
+
+    public void upgradeAcademicCenter(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).increaseLevel();
+        }
+    }
+
+    public void downgradeAcademicCenter(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).decreaseLevel();
+        }
     }
 }
--- src/com/endicott/edu/models/DiningHallModel.java
+++ src/com/endicott/edu/models/DiningHallModel.java
@@ -4,10 +4,25 @@ import java.util.ArrayList;
 
 public class DiningHallModel extends BuildingModel {
     //inherits from BuildingModel
+    public static final int maxLevel = 10;
     private ArrayList<Upgrades> upgrades;
 
     public DiningHallModel(String name, int numStudents, String size){
         super(name, numStudents, BuildingType.dining().getType(), size);
         this.upgrades = DiningHallUpgrades.getUpgrades();
+    }
+
+    public void upgradeDiningHallModel(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).increaseLevel();
+        }
+    }
+
+    public void downgradeDiningHallModel(Upgrades upgrade) {
+        for(int i = 0; i < maxLevel; i++) {
+            if(upgrade.getName()==upgrades.get(i).getName())
+                upgrades.get(i).decreaseLevel();
+        }
     }
 }
