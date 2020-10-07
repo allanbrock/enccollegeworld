2a90060242edf0f8ed12705dca8c1165b1a2be8e
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-08 04:19:25 +0530

    Added a boolean variable to Upgrade to see weather the upgrade affects the interiors or exterior. Changed variable name from level to currentLevel as max level now exists as a seperate variable.

--- src/com/endicott/edu/models/Upgrade.java
+++ src/com/endicott/edu/models/Upgrade.java
@@ -1,32 +1,36 @@
 package com.endicott.edu.models;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Upgrade implements Serializable {
-    private String name;
-    private int cost;
-    private int happinessModifier;
-    private int level;
-    private String description;
-    private ArrayList<UpgradeEvents> events;
-    private int maxLevel;
+    protected String name;
+    protected int cost;
+    protected int happinessModifier;
+    protected int curretLevel;
+    protected String description;
+    protected ArrayList<UpgradeEvents> events;
+    protected int maxLevel;
+    protected boolean isInteriorChange;
 
     public Upgrade(String name, int cost, int happinessModifier){
         this.name = name;
         this.cost = cost;
         this.happinessModifier = happinessModifier;
-        this.level = 0;
+        this.curretLevel = 0;
     }
 
-    public Upgrade(String name, int cost, int happinessModifier, String description, ArrayList<UpgradeEvents> events, int maxLevel){
+    public Upgrade(String name, int cost, int happinessModifier, String description, ArrayList<UpgradeEvents> events, int maxLevel, boolean typeOfChange){
         this.name = name;
         this.cost = cost;
         this.happinessModifier = happinessModifier;
-        this.level = 0;
+        this.curretLevel = 0;
         this.description = description;
         this.events = events;
         this.maxLevel = maxLevel;
+        this.isInteriorChange = typeOfChange;
     }
 
     public String getName(){
@@ -47,12 +51,14 @@ public class Upgrade implements Serializable {
     //gives level to the upgrades, each building may have a max level of upgrade
     //each level increase should give more stats eg. student happiness, faculty happiness, college rating, etc
     public void increaseLevel(){
-        this.level++;
+        this.curretLevel++;
     }
 
-    public void decreaseLevel(){this.level--;}
+    public void decreaseLevel(){this.curretLevel--;}
 
     public int getMaxLevel(){
         return this.maxLevel;
     }
+
+    public Boolean getTypeOfChange(){ return this.isInteriorChange; }
 }
