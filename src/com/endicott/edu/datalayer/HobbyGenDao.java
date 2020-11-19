17373f484520ae89a1d68bee983a586a7c127b10
Matthew Cruz <mcruz772@mail.endicott.edu>
2020-11-19 13:51:02 -0500

    updated Hobbies.

--- src/com/endicott/edu/datalayer/HobbyGenDao.java
+++ src/com/endicott/edu/datalayer/HobbyGenDao.java
@@ -1,20 +1,23 @@
 package com.endicott.edu.datalayer;
 import java.util.Random;
-
+/**
+ * Implemented 11-18-2020 by Matt Cruz
+ * mcruz772@mail.endicott.edu
+ */
 public class HobbyGenDao {
     private static final String[] hobbies = {"Watching Sports","Video Games","Coding","Movies","Reading","Calligraphy","Stamp Collecting",
             "Cooking","Drinking","Writing","Dancing","Singing","Acting","Painting","Kayaking","Camping","Fishing","Fly Fishing",
             "Hunting", "Swimming", "Tanning", "Exercising","Walks on the Beach", "Curling", "Shopping","Smoking","Gardening","Bobsledding",
             "Bird Watching", "Building Legos", "Origami", "Water Polo", "Parkour", "Podcasting", "Eating", "Astrology", "Photography",
-            "Karate", "Judo", "Pankration", "Jellyfishing", "Coloring", "Gambling", "Rubik's Cube", "Driving", "Surfing", "Sport Stacking",
-            "Stand up Comedy", "Golfing", "Pranks", "Magic", "Cleaning", "Filmmaking", "Baking", "Social Media", "Hacking", "Tax evasion",
-            "Puzzles", "Ice Skating", "Anime", "Metal Detecting", "Herping", "Whale Watching", "Rock Climbing", "Marbles", "Beyblades",
-            "Pokémon", "Club Penguin", "Sky Diving", "Car Enthusiast", "Strangling", "Watching Soap Operas", "Netflix", "Folding Laundry",
+            "Karate", "Judo", "Pankration", "Coloring", "Gambling", "Driving", "Surfing", "Sport Stacking",
+            "Stand up Comedy", "Golfing", "Pranks", "Magic", "Cleaning", "Film Making", "Baking", "Social Media", "Hacking",
+            "Puzzles", "Ice Skating", "Anime", "Metal Detecting", "Whale Watching", "Rock Climbing", "Marbles",
+            "Club Penguin", "Sky Diving", "Car Enthusiast", "Watching Soap Operas", "Netflix", "Folding Laundry",
             "Bowling", "Clock Collecting", "Jump Roping", "Knitting", "Yoga", "Traveling", "Music", "Vibing", "Gymnastics", "Skateboarding",
             "Star Gazing", "Jewelry Making", "Poetry", "Farming", "Coffee Making", "Street Racing", "Board Games", "Tie Dyeing"};
 
     /**
-     * This function returns a first and last name, then combines them
+     * This function returns an array of three hobbies,
      * @return a first and last name concat.. with a string.
      */
     public static String[] generateHobbies(){
