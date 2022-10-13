0bf77fe7af14e5b3781cb97dae15f13025bae1f5
Greg Hosking <hoskinggregory@gmail.com>
2022-10-13 14:50:44 -0400

    Refactor StudentsServlet to use new ServletTemplate

--- src/com/endicott/edu/rest/StudentsServlet.java
+++ src/com/endicott/edu/rest/StudentsServlet.java
@@ -13,30 +13,62 @@ import java.util.logging.Logger;
  * Created by Timothy Amello on 10/02/2019.
  */
 
-public class StudentsServlet extends javax.servlet.http.HttpServlet {
-    private static Logger logger = Logger.getLogger("ViewAboutServlet");
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo == null) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            return;
+public class StudentsServlet extends ServletTemplate<StudentModel> {
+    @Override
+    protected StudentModel handleGet(String[] pathSegments) {
+        if (pathSegments.length < 2) {
+            return null;
         }
 
-        String[] splits = pathInfo.split("/");
-        if(splits.length < 2) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            return;
+        String collegeId = pathSegments[0];
+        int id = Integer.parseInt(pathSegments[1]);
+        StudentModel[] students = StudentDao.getStudentsArray(collegeId);
+
+        for (StudentModel student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
         }
 
-        String collegeId = splits[1];
-        StudentModel[] students;
-        students = StudentDao.getStudentsArray(collegeId);
+        return null;
+    }
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    @Override
+    protected StudentModel[] handleGetList(String[] pathSegments) {
+        if (pathSegments.length > 1) {
+            return null;
+        }
 
-        RestHelper.sendAsJson(response, students);
+        String collegeId = pathSegments[0];
+        StudentModel[] students = StudentDao.getStudentsArray(collegeId);
+        return students;
     }
 }
+//
+//public class StudentsServlet extends javax.servlet.http.HttpServlet {
+//    private static Logger logger = Logger.getLogger("ViewAboutServlet");
+//
+//    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+//        String pathInfo = request.getPathInfo();
+//        if (pathInfo == null) {
+//            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        String[] splits = pathInfo.split("/");
+//        if(splits.length < 2) {
+//            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//            return;
+//        }
+//
+//        String collegeId = splits[1];
+//        StudentModel[] students;
+//        students = StudentDao.getStudentsArray(collegeId);
+//
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+//
+//        RestHelper.sendAsJson(response, students);
+//    }
+//}
